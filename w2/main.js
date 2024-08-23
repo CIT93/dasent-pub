2ab22469afa0f4971ea8f5a5f2b87af188aba5c5
dasenthao <dasenthao@hotmail.com>
2024-08-23 02:12:07 +0000

    carbon footprint

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,20 @@
-console.log
+// Calculating my Carbon Footprint
+
+// 1. Number of Household Members
+const HhMember = 4
+// 2. Size of your home
+const HomeSize = 7
+// 3. Evaluate Food Choices
+const FoodChoice = 10
+// 4. Water Consumption
+const WaterIntake = 3
+// 5. Annual Household Purchase
+const HhItem = 6
+// 6. Household Waste Production
+const TrashItem = 20
+// 7. Recyling Waste
+const RecycleItem = 16
+// 8. Total Annual Transportation
+const TransPortation = 14
+// 9. Total Points Earned
+const TtCarbonPoints = HhMember + HomeSize + FoodChoice + WaterIntake + HhItem + TrashItem + RecycleItem + TransPortation

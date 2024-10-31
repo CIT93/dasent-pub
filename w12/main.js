036e0647690f9e1de6f2ac0608c4fb2b46ef2f25
dasenthao <dasenthao@hotmail.com>
2024-10-31 04:12:22 +0000

    w12 my attempt at the code before I review other students code

--- w12/main.js
+++ w12/main.js
@@ -6,13 +6,17 @@ FORM.addEventListener('submit', function (e) {
     const numRep = FORM.numRep.value;
     const workoutTime = FORM.workoutTime.value;
     start(exerciseType, numRep, workoutTime);
+    FORM.reset()
 
 })
 
 function start(exerciseType, numRep, workoutTime){
     const output = document.getElementById("output");
-    const newH2 = document.createElement("h1");
-    newH2.textContent = `Start ${exerciseType} and try to reach ${numRep} in ${workoutTime} seconds.`;
-    setTimeout(() => { `${exerciseType} time is up`;}, workoutTime*1000);
+    const newH1 = document.createElement("h1");
+    const newH2 = document.createElement("h2");
+    newH1.textContent = `Start your ${exerciseType} and try to reach ${numRep} reps in ${workoutTime} seconds.`,
+    setTimeout(() => { newH2.textContent = `${exerciseType} time is up!`;}, workoutTime*1000, )
+    output.appendChild(newH1);
+    output.appendChild(newH2);
 
 };

7539215100d793dc9d149a34e107c18c51b86e36
dasenthao <dasenthao@hotmail.com>
2024-11-08 01:55:34 +0000

    w13 my attempt at the code before I review other students or solution code

--- w13/main.js
+++ w13/main.js
@@ -18,16 +18,20 @@ function start(exerciseType, numRep, workoutTime){
     return new Promise(function (resolve) { 
     const output = document.getElementById("output");
     const newH1 = document.createElement("h1");
-    resolve(newH1.textContent = `Start your ${exerciseType} and try to reach ${numRep} reps in ${workoutTime} seconds.`);
-    output.appendChild(newH1);    
+    newH1.textContent = `Start your ${exerciseType} and try to reach ${numRep} reps in ${workoutTime} seconds.`;
+    output.appendChild(newH1);
+    setTimeout(() => { 
+        const newH2 = document.createElement("h2");
+        output.appendChild(newH2);
+        resolve(newH2.textContent = `${exerciseType} time is up!`)}, workoutTime*1000 )   
     })
 };
 
-function result (exerciseType, workoutTime){
-    return new Promise(function (resolve) {   
+function result(workoutTime){
+    return new Promise(function (resolve) { 
     setTimeout(() => { 
-    const newH2 = document.createElement("h2");
-    output.appendChild(newH2);
-    resolve(newH2.textContent = `${exerciseType} time is up!`)}, workoutTime*1000, )
+        const newH3 = document.createElement("h3");
+        output.appendChild(newH3);
+        resolve(newH3.textContent = "Good workout! 💪")}, workoutTime*1000 )   
     })
 }

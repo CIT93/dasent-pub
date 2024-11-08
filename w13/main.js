3ef924ea3406c911e1d86fd808d4d6141303e8e1
dasenthao <dasenthao@hotmail.com>
2024-11-08 04:04:31 +0000

    my updated code

--- w13/main.js
+++ w13/main.js
@@ -7,6 +7,7 @@ FORM.addEventListener('submit', function (e) {
     const workoutTime = FORM.workoutTime.value;
     start(exerciseType, numRep, workoutTime)
     .then(result)
+    .catch(onError)
 
     FORM.reset()
 
@@ -15,7 +16,10 @@ FORM.addEventListener('submit', function (e) {
 
 
 function start(exerciseType, numRep, workoutTime){
-    return new Promise(function (resolve) { 
+    return new Promise((function (resolve, reject) { 
+        if (workoutTime === 0) {
+            reject("Error on Time Selection");      
+        } else {
     const output = document.getElementById("output");
     const newH1 = document.createElement("h1");
     newH1.textContent = `Start your ${exerciseType} and try to reach ${numRep} reps in ${workoutTime} seconds.`;
@@ -24,8 +28,11 @@ function start(exerciseType, numRep, workoutTime){
         const newH2 = document.createElement("h2");
         output.appendChild(newH2);
         resolve(newH2.textContent = `${exerciseType} time is up!`)}, workoutTime*1000 )   
-    })
+}
+    }));
 };
+
+
 
 function result(workoutTime){
     return new Promise(function (resolve) { 
@@ -34,4 +41,9 @@ function result(workoutTime){
         output.appendChild(newH3);
         resolve(newH3.textContent = "Good workout! 💪")}, workoutTime*1000 )   
     })
+}
+
+const onError = (error)=> {
+
+    console.log(error);
 }

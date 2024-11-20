8feaeec29c6f26f92ea3a00e0febb056118da272
dasenthao <dasenthao@hotmail.com>
2024-11-20 02:38:26 +0000

    w15 code challenge starting code

---
+++ w15/fp.js
@@ -0,0 +1,62 @@
+class FP {
+    constructor(first, last, houseMembers, houseSize, foodChoice) {
+        this.first = first;
+        this.last = last;
+        this.houseMembers = houseMembers;
+        this.houseSize = houseSize;
+        this.foodChoice = foodChoice
+        this.houseHoldPoints();
+        this.houseSizePoints();
+        this.foodCarbonTotal();
+        this.total();
+    }
+
+    houseHoldPoints() {
+        if (this.houseMembers === 1) {
+            this.houseHoldPoints = 14;
+        } else if (this.houseMembers === 2) {
+            this.houseHoldPoints = 12;
+        } else if (this.houseMembers === 3) {
+            this.houseHoldPoints = 10;
+        } else if (this.houseMembers === 4) {
+            this.houseHoldPoints = 8;
+        } else if (this.houseMembers === 5) {
+            this.houseHoldPoints = 6;
+        } else if (this.houseMembers === 6) {
+            this.houseHoldPoints = 4;
+        } else if (this.houseMembers > 6) {
+            this.houseHoldPoints = 2;
+        }
+    }
+    houseSizePoints() {
+        if (this.houseSize === "large house") {
+            this.houseSizePoints = 10;
+        } else if (this.houseSize === "medium house") {
+            this.houseSizePoints = 7;
+        } else if (this.houseSize === "small house") {
+            this.houseSizePoints = 4;
+        } else if (this.houseSize === "apartment") {
+            this.houseSizePoints = 2;
+        }
+    }
+    
+    foodCarbonTotal() {
+        if (this.foodChoice === "meat daily basis") {
+            this.foodCarbonTotal = 10;
+        } else if (this.foodChoice === "meat few times per week") {
+            this.foodCarbonTotal = 8;
+        } else if (this.foodChoice === "vegetarian") {
+            this.foodCarbonTotal = 4;
+        } else if (this.foodChoice === "vegan or wild meat") {
+            this.foodCarbonTotal = 2;
+        }
+
+    }
+
+    total() {
+        this.total = this.houseHoldPoints + this.houseSizePoints + this.foodCarbonTotal;
+
+    }
+}
+
+export { FP }

8db92ffb01ebbe20f75c70b51f8c8ebb94a29218
dasenthao <dasenthao@hotmail.com>
2024-12-01 21:32:06 +0000

    w16 setup done

---
+++ w16/fp.js
@@ -0,0 +1,76 @@
+class FP {
+    constructor(first, last, houseMembers, houseSize, foodChoice, foodSource, waterConsum) {
+        this.first = first;
+        this.last = last;
+        this.houseMembers = houseMembers;
+        this.houseSize = houseSize;
+        this.foodChoice = foodChoice;
+        this.foodSource = foodSource;
+        this.waterConsumPoints = waterConsum;
+        this.calHouseHoldPoints();
+        this.calHouseSizePoints();
+        this.calFoodCarbonTotal();
+        this.calFoodSourcePoints();
+        this.calTotal();
+    }
+
+    calHouseHoldPoints() {
+        if (this.houseMembers === 1) {
+            this.houseHoldPoints = 14;
+        } else if (this.houseMembers === 2) {
+            this.houseHoldPoints = 12;
+        } else if (this.houseMembers === 3) {
+            this.houseHoldPoints = 10;
+        } else if (this.houseMembers === 4) {
+            this.houseHoldPoints = 8;
+        } else if (this.houseMembers === 5) {
+            this.houseHoldPoints = 6;
+        } else if (this.houseMembers === 6) {
+            this.houseHoldPoints = 4;
+        } else if (this.houseMembers > 6) {
+            this.houseHoldPoints = 2;
+        }
+    }
+    calHouseSizePoints() {
+        if (this.houseSize === "large house") {
+            this.houseSizePoints = 10;
+        } else if (this.houseSize === "medium house") {
+            this.houseSizePoints = 7;
+        } else if (this.houseSize === "small house") {
+            this.houseSizePoints = 4;
+        } else if (this.houseSize === "apartment") {
+            this.houseSizePoints = 2;
+        }
+    }
+
+    calFoodCarbonTotal() {
+        if (this.foodChoice === "meat daily basis") {
+            this.foodCarbonTotal = 10;
+        } else if (this.foodChoice === "meat few times per week") {
+            this.foodCarbonTotal = 8;
+        } else if (this.foodChoice === "vegetarian") {
+            this.foodCarbonTotal = 4;
+        } else if (this.foodChoice === "vegan or wild meat") {
+            this.foodCarbonTotal = 2;
+        }
+
+    }
+
+    calFoodSourcePoints() {
+        if (this.foodSource === "packed") {
+            this.foodSourcePoints = 12;
+        } else if (this.foodSource === "balance") {
+            this.foodSourcePoints = 6;
+        } else if (this.foodSource === "local") {
+            this.foodSourcePoints = 2;
+        }
+
+    }
+
+    calTotal() {
+        this.total = this.houseHoldPoints + this.houseSizePoints + this.foodCarbonTotal + this.foodSourcePoints + this.waterConsumPoints;
+
+    }
+}
+
+export { FP }
